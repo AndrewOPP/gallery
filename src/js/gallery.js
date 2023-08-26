d940f2e7d03a05d46e7c2d6da3d2989c2641d6a4
Olegmkv <129410354+Olegmkv@users.noreply.github.com>
2023-08-26 13:09:38 +0300

    search

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -2,8 +2,9 @@ import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
 import { createGalleryCard } from './creat';
 import { UnsplashAPI } from "./UnsplashAPI";
-const list = document.querySelector('.js-gallery');
 
+const list = document.querySelector('.js-gallery');
+const form=document.querySelector('.js-search-form')
 
 const options = {
     totalItems: 0,
@@ -27,7 +28,9 @@ api.getPopularPhoto(page)
     });
 
 
-pagination.on('afterMove', (event) => {
+pagination.on('afterMove', getPopular);
+
+function getPopular(event) {
      const currentPage = event.page;
      api.getPopularPhoto(currentPage)
      .then(({ total, results }) => {
@@ -36,6 +39,38 @@ pagination.on('afterMove', (event) => {
          list.innerHTML = markup;
      });
 
-});
+};
+    
+form.addEventListener('submit', onSearchForm);
 
+function onSearchForm(evt) {
+    evt.preventDefault();
+    const { query } = evt.target.elements;
+    const searchValue = query.value.trim();
+    api.query = searchValue;
+    pagination.off('afterMove', getPopular);
+    pagination.off('afterMove', getByQuery);
+    api.getPhotoByQuery(page)
+    .then(({ total, results }) => {
 
+        pagination.reset(total);
+        console.log(results);
+        const markup = createGalleryCard(results);
+        list.innerHTML = markup;
+
+    });
+ 
+pagination.on('afterMove', getByQuery);
+}
+
+function getByQuery(event) {
+
+     const currentPage = event.page;
+     api.getPhotoByQuery(currentPage)
+     .then(({ total, results }) => {
+         console.log(results);
+         const markup = createGalleryCard(results);
+         list.innerHTML = markup;
+     });
+
+};
