32fe4635f52643898ff068553aff3071bb444871
kait-meshcheriakova <129512123+kait-meshcheriakova@users.noreply.github.com>
2023-08-26 12:07:02 +0300

    add fetch

---
+++ src/js/UnsplashAPI.js
@@ -0,0 +1,14 @@
+export class UnsplashAPI{
+    #BASE_URL="https://api.unsplash.com/search/photos";
+    #KEY = "gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58";
+    getPopularPhoto(page){
+    const url = `${this.#BASE_URL}?query=popular&page=${page}&per_page=12&orientation=portrait&client_id=${this.#KEY}`;
+    return fetch(url).then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .catch(error=>console.log(error))
+    }
+}
