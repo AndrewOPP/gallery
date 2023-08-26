799afed2756d362d50336f53b00558e45e8fd9cd
DenysShchypt <129331185+DenysShchypt@users.noreply.github.com>
2023-08-26 09:12:20 +0000

    Deploying to gh-pages from @ AndrewOPP/gallery@0302f39150a81540734480937ad2890e7fadbdef 🚀

--- index.10413618.js
+++
@@ -1,2 +0,0 @@
-!function(){var n,e=Number(prompt("Enter something"));(n=e,new Promise((function(e,o){Number.isNaN(n)&&o("Error"),n%2==0&&setTimeout((function(){return e("even")}),1e3),n%2!=0&&setTimeout((function(){return e("odd")}),2e3)}))).then((function(n){return console.log(n)})).catch((function(n){return console.log(n)}))}();
-//# sourceMappingURL=index.10413618.js.map
